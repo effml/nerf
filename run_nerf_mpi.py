9d5f5513ade9e7123040f2f9c081c01760909498
Alexander Kristoffersen <akristoffersen@berkeley.edu>
2022-04-07 14:05:56 -0700

    cleaned up run_nerf_mpi.py

--- run_nerf_mpi.py
+++ run_nerf_mpi.py
@@ -17,9 +17,9 @@ import run_nerf
 
 tf.compat.v1.enable_eager_execution()
 
-def render_plane(H, W, focal,
+def render_planes(H, W, focal,
            chunk=1024*32, rays=None, c2w=None, ndc=True,
-           near_plane_depth=0., far_plane_depth=1.,
+           plane_depths=None, num_planes=12,
            use_viewdirs=False, c2w_staticcam=None,
            **kwargs):
     """Render rays
@@ -47,54 +47,18 @@ def render_plane(H, W, focal,
       extras: dict with everything returned by render_rays().
     """
 
-    if c2w is not None:
-        # special case to render full image
-        rays_o, rays_d = get_rays(H, W, focal, c2w)
+    if plane_depths is None:
+        plane_depths = np.linspace(0.0, 1.0, num_planes)
     else:
-        # use provided ray batch
-        rays_o, rays_d = rays
+        num_planes = len(plane_depths)
+    for i in range(num_planes - 1):
+        near_depth = plane_depths[i]
+        far_depth = plane_depths[i + 1]
 
-    if use_viewdirs:
-        # provide ray directions as input
-        viewdirs = rays_d
-        if c2w_staticcam is not None:
-            # special case to visualize effect of viewdirs
-            rays_o, rays_d = get_rays(H, W, focal, c2w_staticcam)
-
-        # Make all directions unit magnitude.
-        # shape: [batch_size, 3]
-        viewdirs = viewdirs / tf.linalg.norm(viewdirs, axis=-1, keepdims=True)
-        viewdirs = tf.cast(tf.reshape(viewdirs, [-1, 3]), dtype=tf.float32)
-
-    sh = rays_d.shape  # [..., 3]
-    if ndc:
-        # for forward facing scenes
-        rays_o, rays_d = ndc_rays(
-            H, W, focal, tf.cast(1., tf.float32), rays_o, rays_d)
-
-    # Create ray batch
-    rays_o = tf.cast(tf.reshape(rays_o, [-1, 3]), dtype=tf.float32)
-    rays_d = tf.cast(tf.reshape(rays_d, [-1, 3]), dtype=tf.float32)
-
-    print(rays_d)
-    print(rays_o)
+        plane_outputs = run_nerf.render(
+            H, W, focal, chunk=chunk, rays=rays, c2w=c2w, 
+            ndc=ndc, near=near_depth, far=far_depth, 
+            use_viewdirs=use_viewdirs, c2w_staticcam=c2w_staticcam,
+        )
     
-    near, far = near * \
-        tf.ones_like(rays_d[..., :1]), far * tf.ones_like(rays_d[..., :1])
-
-    # (ray origin, ray direction, min dist, max dist) for each ray
-    rays = tf.concat([rays_o, rays_d, near, far], axis=-1)
-    if use_viewdirs:
-        # (ray origin, ray direction, min dist, max dist, normalized viewing direction)
-        rays = tf.concat([rays, viewdirs], axis=-1)
-
-    # Render and reshape
-    all_ret = run_nerf.batchify_rays(rays, chunk, **kwargs)
-    for k in all_ret:
-        k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
-        all_ret[k] = tf.reshape(all_ret[k], k_sh)
-
-    k_extract = ['rgb_map', 'disp_map', 'acc_map']
-    ret_list = [all_ret[k] for k in k_extract]
-    ret_dict = {k: all_ret[k] for k in all_ret if k not in k_extract}
-    return ret_list + [ret_dict]
+        # do something with this
